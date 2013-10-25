0dc91d389228a7f1e6b6f25de66121af944863e3
Julien <Julien@Bobaf-eight>
2013-10-25 18:58:19 +0200

    MySQL insteads of json files, Flat-UI 2.x with Bootstrap 3

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <?php include('struct/header.html'); ?>
-<div class="headline">
-    <h1>
-        <div class="logo"></div>
-        Rasp Pie
-        <small>Tool for monitoring Raspberry Pi</small>
-    </h1>
-</div>
-<div class="row">
-    <div class="col-md-6 col-md-offset-3">	
-        <form class="login-form" method="post" action="home.php">
-            <div class="form-group">
-                <input type="text" id="login-name" placeholder="Enter your name" value="" class="form-control login-field">
-                <label for="login-name" class="login-field-icon fui-user"></label>
-            </div>
+	<div class="demo-headline" id="raysDemoHolder">
+        <h1 class="demo-logo">
+          <div class="logo"></div>
+          Rasp Pie
+          <small>Tool for monitoring Raspberry Pi</small>
+        </h1>
+	</div>
+	
+	<!-- Login form -->
+	<div class="row">
+		<div class="offset3 span6 offset3">
+			<form class="login-form" method="post" action="home.php">
+				<fieldset>
+					<div class="control-group">
+					  <input type="text" required placeholder="Enter your name" class="login-field">
+					  <label for="login-name" class="login-field-icon fui-user"></label>
+					</div>
 
-            <div class="form-group">
-                <input type="password" id="login-pass" placeholder="Password" value="" class="form-control login-field">
-                <label for="login-pass" class="login-field-icon fui-lock"></label>
-            </div>
+					<div class="control-group">
+					  <input type="password" required placeholder="Password" class="login-field">
+					  <label for="login-pass" class="login-field-icon fui-lock"></label>
+					</div>
 
-            <button type="submit"class="btn btn-primary btn-lg btn-block">Login</button>
-
-            <a href="#" class="login-link">Lost your password?</a>
-        </form>
-    </div>
-</div>
-
+					<button type="submit"class="btn btn-primary btn-large btn-block">Login</button>
+					
+					<a href="#" class="login-link">Lost your password?</a>
+				</fieldset>
+			</form>
+		</div>
+		
+	</div>
 
 
 <?php include('struct/footer.html'); ?>
